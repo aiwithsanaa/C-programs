915d3a3c0fbadfbd46918edcce8e28efe0f17b46
Sanaa <khansanaa418@gmail.com>
2026-08-06 21:25:03 +0530

    Added swapping programs in C

---
+++ with-third-veriable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,temp;
+    printf("Enter the value of A: ");
+    scanf("%d",&a);
+    printf("Enter the value of B: ");
+    scanf("%d",&b);
+    printf("After Swapping we have:-\n");
+    temp=a;
+    a=b;
+    b=temp;
+    printf("Value of A: %d\n",a);
+    printf("Value of B: %d",b);
+    return 0;
+}
