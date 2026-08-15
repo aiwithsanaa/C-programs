7b7d0a0e3f45737031e5072adeeda60940b59130
Sanaa <khansanaa418@gmail.com>
2026-08-15 20:14:03 +0530

    DAY 6:Sum of n natural numbers.

---
+++ sumof1-n.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i);
+        sum=sum+i;
+        if(i<n)
+        printf(" + ");
+    }
+    printf(" = %d",sum);
+    return 0;
+}
